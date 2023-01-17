8c53dd854dc7c5c925f142760d1cc4f9982a49b7
Oguzhan Ozdemir <aoozdemir@live.com>
2023-01-17 15:19:00 +0300

    Add necessary S3 bucket to the infra stack

--- infra/app/bin/app.ts
+++ infra/app/bin/app.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { Stack } from 'aws-cdk-lib';
 import { SandboxELBStack } from '../lib/elb/sandbox-elb-stack';
+import { S3BucketsStack } from '../lib/s3/s3-buckets-stack';
 
 const result = require('dotenv').config({ path: `../${process.env.PROFILE}/.env` });
 if (result.error) {
@@ -25,3 +26,9 @@ const mainStack = new Stack(app, `sidekick-sandbox-setup-stack-${process.env.STA
 // ELB Stack
 //
 new SandboxELBStack(mainStack, `sidekick-sandbox-elb-stack-${process.env.STAGE}`, {});
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// S3 Stack
+//
+new S3BucketsStack(mainStack, `sidekick-sandbox-s3-stack-${process.env.STAGE}`);
---
+++ infra/app/lib/s3/s3-buckets-stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class S3BucketsStack extends cdk.NestedStack {
+
+    constructor(scope: Construct, id: string, props?: cdk.NestedStackProps) {
+        super(scope, id, props);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        //
+        // S3 Buckets
+        //
+        new s3.Bucket(this, `sidekick-todo-app-${process.env.STAGE}`, {
+            bucketName: `sidekick-todo-app-${process.env.STAGE}`,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
+    }
+}
